51248992a6c15cd61c581d4eb3de768d92820e2a
Дюша <58334576+Menxy@users.noreply.github.com>
2024-02-29 11:37:58 +0200

    2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import ReactDOM from 'react-dom/client';
-import {RouterProvider} from "react-router-dom";
-
 import './index.css';
-import {router} from "./router";
+import {App} from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <RouterProvider router={router}/>
+    <App/>
 );
 
